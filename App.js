b14efff427e5ac8e15bf6876aa82901a19903a28
bandiEswar <121429247+bandiEswar@users.noreply.github.com>
2023-07-31 10:21:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Cal from './Components/Calci';
+import Calci from './Components/Calci';
+
+function App() {
+  return (
+    <div className="App">
+      <Calci />
+    </div>
+  );
+}
+
+export default App;
